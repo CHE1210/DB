821f125842433aeb1f9bd39a3cc243beb15610fe
LCH1210 <dldial0902@gmail.com>
2025-06-15 21:59:32 +0900

    Add files via upload

---
+++ DB 필수 과제.sql
@@ -0,0 +1,9 @@
+-- USE testdatabase;
+
+-- CREATE TABLE emloyees (
+-- 	user_id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(100),
+--     position VARCHAR(100),
+--     salary DECIMAL(10,2)
+-- );
+
